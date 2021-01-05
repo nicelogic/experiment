f5ec4a888a6eb8615cf87082be1c52f4522d451f
bryan.wu <bryan.wu@ringcentral.com>
2021-01-05 20:40:03 +0800

    1

--- cpp/google-test/google-test.cpp
+++ cpp/google-test/google-test.cpp
@@ -1,62 +1,38 @@
 
 #include "gtest/gtest.h"
+#include <iostream>
 
-//simple test
-int fun()
+using namespace std;
+
+bool fun(int ival)
 {
-    return 0;
+    return false;
 }
 
 TEST(fun, funtest)
 {
-    EXPECT_EQ(fun(), 0);
+    SUCCEED();
 }
 
-template <typename E> // E is the element type.
-class Queue
-{
-public:
-    Queue(){};
-    void Enqueue(const E &element){}
-    E *Dequeue() { return nullptr; } // Returns NULL if the queue is empty.
-    size_t size() const {return 0;}
-};
-
-class QueueTest : public ::testing::Test
+class BaseTestFixture : public testing::Test
 {
 protected:
     void SetUp() override
     {
-        q1_.Enqueue(1);
-        q2_.Enqueue(2);
-        q2_.Enqueue(3);
+        cout << "test base fixture" << endl;
     }
-
-    // void TearDown() override {}
-
-    Queue<int> q0_;
-    Queue<int> q1_;
-    Queue<int> q2_;
 };
 
+class DerivedTestFixture : public BaseTestFixture
+{
+protected:
+    void SetUp() override
+    {
+        cout << "derived fixture" << endl;
+    }
+};
 
-TEST_F(QueueTest, IsEmptyInitially) {
-  EXPECT_EQ(q0_.size(), 0);
-}
-
-TEST_F(QueueTest, DequeueWorks) {
-  int* n = q0_.Dequeue();
-  EXPECT_EQ(n, nullptr);
-
-  n = q1_.Dequeue();
-  EXPECT_NE(n, nullptr);
-  EXPECT_EQ(*n, 1);
-  EXPECT_EQ(q1_.size(), 0);
-  delete n;
-
-  n = q2_.Dequeue();
-  ASSERT_NE(n, nullptr);
-  EXPECT_EQ(*n, 2);
-  EXPECT_EQ(q2_.size(), 1);
-  delete n;
+TEST_F(DerivedTestFixture, JustTest)
+{
+    SUCCEED();
 }
